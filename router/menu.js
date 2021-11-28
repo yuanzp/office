ea77cc136a53b4e621fd75a9f5010eb0eca5fe36
yuanzhaoping <eyuanzp@hotmail.com>
2021-11-28 16:15:25 +0800

    完成后台管理模块

--- router/menu.js
+++ router/menu.js
@@ -1,16 +1,53 @@
+const { ObjectId } = require("bson");
 const express = require("express");
 const router = express.Router();
-const menuModel=require('../model/menuModel')
+const menuModel = require('../model/menuModel')
 const table = require("../model/table")
 
-router.get("/my",(req,res)=>{
-    res.send({
-        code:1,
-        data:menuModel.menus
+router.get("/my", (req, res) => {
+    let user = JSON.parse(req.headers['gw-user']);
+    table.get("user").then(userTable => {
+        userTable.find({ UserId: user.ID }).toArray().then(result => {
+            if (result.length > 0) {
+                let role = result[0].role == null ? {
+                    _id: '619e527359a172ebedd61290',
+                    name: 'base'
+                } : result[0].role;
+
+                table.get("role").then(roleTable => {
+                    roleTable.find({ _id: ObjectId(role._id) }).toArray().then(result => {
+                        let menuIds = result[0].menus;
+
+                        table.get("menu").then(menuTable => {
+                            menuTable.find().toArray({ _id: { $in: menuIds } }).then(result => {
+                                let menus = result;
+                                let treeMenu = getTreeMenu(menus);
+
+                                res.send({
+                                    code: 1,
+                                    data: treeMenu
+                                })
+                            })
+                        })
+                    })
+                })
+            }
+            else {
+                res.send({
+                    code: 1,
+                    data: menuModel.menus
+                })
+            }
+        })
     })
+
+    // res.send({
+    //     code: 1,
+    //     data: menuModel.menus
+    // })
 })
 
-router.get("/", (req, res) => {    
+router.get("/", (req, res) => {
     table.get("menu").then((menuTable) => {
         menuTable.find().sort({ order: 1 }).toArray().then((result) => {
             res.send({
@@ -21,12 +58,12 @@ router.get("/", (req, res) => {
     })
 })
 
-router.post("/",(req,res)=>{
-    var menu=req.body;
+router.post("/", (req, res) => {
+    var menu = req.body;
 
     table.get("menu").then((menuTable) => {
         menuTable.insertOne(menu).then((result) => {
-            menu._id=result.insertedId.toString()
+            menu._id = result.insertedId.toString()
             res.send({
                 code: 1,
                 data: menu
@@ -35,18 +72,69 @@ router.post("/",(req,res)=>{
     })
 })
 
-
-
-
-router.get("/role/:id",(req,res)=>{
-    let _id=req.params.id;
-
-    res.send({
-        code:0,
-        data:_id,
-        message:'尚未实现'
+router.put("/", (req, res) => {
+    var menu = req.body;
+    let _id=menu._id;
+    delete menu._id;
+    table.get("menu").then((menuTable) => {
+        menuTable.updateOne({_id:ObjectId(_id)},{$set:menu}).then((result) => {
+            
+            res.send({
+                code: 1,
+                data: result.modifiedCount
+            })
+        })
     })
 })
+
+
+
+router.get("/role/:id", (req, res) => {
+    let _id = req.params.id;
+
+    res.send({
+        code: 0,
+        data: _id,
+        message: '尚未实现'
+    })
+})
+
+
+function getTreeMenu(menus) {
+    let ms = [];
+    for (let index = 0; index < menus.length; index++) {
+        const menu = menus[index];
+        if (menu.parentId == 0) {
+            let m = {
+                id: menu._id,
+                parentId: menu.parentId,
+                label: menu.title,
+                path: menu.path,
+                children: []
+            };
+            ms.push(m);
+            addSubMenu(menus, m);
+        }
+    }
+    return ms;
+}
+
+function addSubMenu(menus, menu) {
+    for (let index = 0; index < menus.length; index++) {
+        const subMenu = menus[index];
+        if (subMenu.parentId == menu.id) {
+            let m = {
+                id: subMenu._id,
+                parentId: subMenu.parentId,
+                label: subMenu.title,
+                path: subMenu.path,
+                children: []
+            };
+            menu.children.push(m);
+            addSubMenu(menus, m);
+        }
+    }
+}
 
 
 module.exports = router;
